e787121cd1360d4e05d64c950bc950e8a04d3783
bambamboole <manuel.christlieb@xentral.com>
2022-07-10 19:36:47 +0200

    Build simple vlucas/phpdotenv based EnvironmentLoader

---
+++ src/Environment/EnvironmentLoader.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bambamboole\Framework\Environment;
+
+use Dotenv\Dotenv;
+use Dotenv\Repository\Adapter\PutenvAdapter;
+use Dotenv\Repository\RepositoryBuilder;
+
+class EnvironmentLoader
+{
+    public function load(array $filePaths, array $names): Environment
+    {
+        $builder = RepositoryBuilder::createWithDefaultAdapters();
+        $builder = $builder->addAdapter(PutenvAdapter::class);
+        $repository = $builder->make();
+        $env = Dotenv::create($repository, $filePaths, $names, false);
+
+        return new Environment($env->safeLoad());
+    }
+}
---
+++ tests/Integration/Environment/EnvironmentLoaderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Bambamboole\Framework\Integration\Environment;
+
+use Bambamboole\Framework\Environment\EnvironmentLoader;
+use PHPUnit\Framework\TestCase;
+
+class EnvironmentLoaderTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        $_SERVER = [];
+        $_ENV = [];
+    }
+
+    public function testItLoadsValuesFromEnvFiles(): void
+    {
+        $loader = new EnvironmentLoader();
+
+        $env = $loader->load([__DIR__ . '/fixtures'], ['.env']);
+
+        // FOO is a key in the loaded .env file
+        self::assertEquals('bar', $env->get('FOO'));
+    }
+
+    public function testItCanLoadMultipleFilesFromTheSamePath(): void
+    {
+        $loader = new EnvironmentLoader();
+
+        $env = $loader->load([__DIR__ . '/fixtures'], ['.env', '.env-2']);
+
+        self::assertEquals('bar', $env->get('FOO'));
+        // BAR is the key in the loaded .env-2 file
+        self::assertEquals('baz', $env->get('BAR'));
+    }
+}
